0f57558215fbb5f9d9b48e884fe4bf42003a1b23
borra <ryle2724@gmail.com>
2022-06-29 18:17:48 +0900

    [홈, 디바이스 설정] 펌웨어 버전 체크 수정

--- picoApp_apple-master-6/App/Main/Home/RealTime/Home.js
+++ picoApp_apple-master-6/App/Main/Home/RealTime/Home.js
@@ -76,22 +76,23 @@ export const Home = ({navigation}) => {
   const [isFirmwareUpdate, setIsFirmwareUpdate] = useStateCallbackAfter500(false);
   const [isForcedLogout, setIsForcedLogout] = useStateCallbackAfter500(false);
   
-  const [getLatestFirmwareVersion, getDeviceFirmwareVersion] = useCheckFirmwareVersion()
+  const [getLatestFirmwareVersion, getDeviceFirmwareVersion, compareVersion] = useCheckFirmwareVersion()
 
   const compareFirmwareVersion = async (id) => {
     const latestVersion = await getLatestFirmwareVersion(id)
-    const verson = await getDeviceFirmwareVersion(id)
-
-    return ((latestVersion > verson) || (latestVersion < verson))
+    const version = await getDeviceFirmwareVersion(id)
+    return compareVersion(latestVersion, version)
   }
   const isShowFirmwareUpdate = async () => {
-    let isLatestVersion = true
+    let isNeedFirmwareUpdate = false
 
-    await Promise.all(devices.map(async (_, id) => {
-      isLatestVersion = await compareFirmwareVersion(id)
-    }))
-    
-    setIsFirmwareUpdate(!isLatestVersion)
+    for(let i = 0; i < devices.length; i++) {
+      isNeedFirmwareUpdate = await compareFirmwareVersion(i)    
+      if (isNeedFirmwareUpdate)
+        break;
+    }
+
+    setIsFirmwareUpdate(isNeedFirmwareUpdate)
   }
 
   function open_WhatsApp() {
